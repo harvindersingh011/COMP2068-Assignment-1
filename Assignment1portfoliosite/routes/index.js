9aa621f59e13ccf9257008516b0289b891a707c6
Harvinder Singh <43422594+harvindersingh011@users.noreply.github.com>
2020-06-29 21:27:07 -0400

    final commit

--- Assignment1portfoliosite/routes/index.js
+++ Assignment1portfoliosite/routes/index.js
@@ -2,27 +2,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+/* getting home page*/
 router.get('/home', function (req, res) {
     res.render('home', { home: 'Home Page' });
 });
 
+/* getting service page*/
 router.get('/service', function (req, res) {
     res.render('service', { service: 'Service Page' });
 });
 
+/* getting project page*/
 router.get('/project', function (req, res) {
     res.render('project', { project: 'Project Page' });
 });
 
+/* getting contact me page*/
 router.get('/contactme', function (req, res) {
     res.render('contactme', { contactme: 'Contact Page' });
 });
 
+/* getting aboutme page*/
 router.get('/aboutme', function (req, res) {
     res.render('aboutme', { aboutme: 'Aboutme Page' });
 });
